12e30bb77a7e8bf5a86363b15d5fe2c0f3f1afe6
Pradeep Kumar <pk6949137@gmail.com>
2026-08-11 16:57:40 +0530

    setup inngest and pinecone

--- apps/web/app/api/inngest/route.ts
+++ apps/web/app/api/inngest/route.ts
@@ -1,7 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
+import { processTask } from "@/inngest/function/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [processTask],
 });
--- apps/web/inngest/client.ts
+++ apps/web/inngest/client.ts
@@ -1,4 +1,4 @@
 // src/inngest/client.ts
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "code-rabbit" });
+export const inngest = new Inngest({ id: "code-rabbit-monorepo" });
