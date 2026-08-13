0b44097d0b7a46d99d3288c5fe8b7aca82015b90
Pradeep Kumar <pk6949137@gmail.com>
2026-08-13 10:20:45 +0530

    add generateReview function

--- apps/web/app/api/inngest/route.ts
+++ apps/web/app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { indexRepo, processTask } from "@/inngest/function/inngest";
+import { generateReview } from "@/inngest/function/review";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processTask, indexRepo],
+  functions: [processTask, indexRepo, generateReview],
 });
