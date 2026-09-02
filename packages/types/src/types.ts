68ac2daee90af36de72f87fd05affc6586338c75
Pradeep Kumar <pk6949137@gmail.com>
2026-09-02 22:06:11 +0530

    edit types.ts

--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -29,4 +29,5 @@ export interface Repository {
  language: string | null;
  topics: string[];
 isConnected?: boolean;
+indexingStatus?: "idle" | "pending" | "processing" | "completed" | "failed";
 }
